1dc14c06525c907cb19c6a09588a39ea3d9342bc
Lyaga varshitha <lyagavarshithareddy@gmail.com>
2026-02-25 09:59:57 +0530

    Fix division output formatting in Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -11,6 +11,6 @@ int r4=n1/n2;
 System.out.println("Addition:"+r1);
 System.out.println("Subtraction:"+r2);
 System.out.println("Multiplication:"+r3);
-System.out.println("Division",+r4);
+System.out.println("Division"+r4);
 }
 }
